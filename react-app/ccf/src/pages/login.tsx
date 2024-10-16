bef462ae3faa6d7bd61e4d598203c8e7a9629fa1
eileendchen <eileendchen@gmail.com>
2024-10-16 17:43:44 -0400

    style login and signup pages

--- react-app/ccf/src/pages/login.tsx
+++ react-app/ccf/src/pages/login.tsx
@@ -11,21 +11,18 @@ function Login() {
   const [input, setInput] = useState<InputState>({ email: "", password: "" });
   const [error, setError] = useState<string | null>(null);
 
-  // initialized auth instance
   const auth = getAuth();
 
-  // handle form submit
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setError("");
     const email = input.email.toLowerCase().trim();
     const password = input.password;
 
-    // sign in user
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        // signed in
         console.log(userCredential.user);
+        console.log("Successfully signed in");
       })
       .catch((err) => {
         if (
@@ -47,51 +44,129 @@ function Login() {
     }));
   };
 
+  const styles: { [key: string]: React.CSSProperties } = {
+    container: {
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: '100vh',
+      padding: '20px',
+      fontFamily: 'Roboto, sans-serif',
+      rowGap: '50px',
+    },
+    form: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      width: '60%',
+      backgroundColor: '#fff',
+      padding: '20px',
+      marginRight: '20px',
+    },
+    
+    input: {
+      width: '90%',
+      padding: '10px',
+      marginTop: '10px',
+      marginBottom: '20px',
+      border: '1px solid #ccc',
+      borderRadius: '4px',
+    },
+
+    button: {
+      width: '95%',
+      padding: '10px',
+      backgroundColor: '#BE0019',
+      color: '#fff',
+      border: 'none',
+      borderRadius: '20px',
+      cursor: 'pointer',
+      marginTop: '10px',
+      marginBottom: '10px',
+    },
+    error: {
+      color: '#BE0019',
+      marginBottom: '10px',
+    },
+    rememberMe: {
+      margin: '10px 0',
+    },
+    imageContainer: {
+      flex: 1,
+      height: '70%',
+      backgroundImage: 'url("/login.png")',
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      borderRadius: '8px',
+      maxWidth: '40%',
+      marginTop: '20px', 
+    },
+    heading: {
+      textAlign: 'center',
+      color: '#BE0019',
+    },
+
+    spacing: {
+      marginTop: '20px',
+    },
+
+    logo: {
+      textAlign: 'center',
+      marginBottom: '1rem',
+    },
+    logoImage: {
+      maxWidth: '100px',
+    },
+
+  };
+
   return (
-    <div className="form-body">
-      <form autoComplete="off" className="form" onSubmit={handleSubmit}>
-        <h1>Sign In</h1>
-        <p>Fill the form below to sign in to your account.</p>
-        <div className="email-input">
-          <input
-            name="email"
-            placeholder="Enter email"
-            type="text"
-            onChange={handleChange}
-            value={input.email}
-            required
-            autoComplete="true"
-          />
-          <label htmlFor="email" className="label-name">
-            <span className="content-name">Email</span>
-          </label>
+    <div style={styles.container}>
+      <form autoComplete="off" style={styles.form} onSubmit={handleSubmit}>
+        <div style={styles.logo}>
+          <img src="/ccflogo.png" alt="Logo" style={styles.logoImage} />
         </div>
-        <div className="password-input">
-          <input
-            name="password"
-            placeholder="Enter password"
-            onChange={handleChange}
-            value={input.password}
-            type="password"
-            required
-            autoComplete="true"
-          />
-          <label htmlFor="password" className="label-name">
-            <span className="content-name">Password</span>
-          </label>
+        <h1 style={styles.heading}>Welcome!</h1>
+        <p>New to Children’s Cancer Foundation? <Link to="/signup">Sign Up</Link></p>
+        <label htmlFor="email" className="label-name">
+          <span className="content-name">Email</span>
+        </label>
+        <input
+          name="email"
+          placeholder="Enter your email"
+          type="text"
+          onChange={handleChange}
+          value={input.email}
+          required
+          style={styles.input}
+        />
+        <label htmlFor="password" className="label-name">
+          <span className="content-name">Password</span>
+        </label>
+        <input
+          name="password"
+          placeholder="Enter your password"
+          onChange={handleChange}
+          value={input.password}
+          type="password"
+          required
+          style={styles.input}
+        />
+        <div style={styles.rememberMe}>
+          <input type="checkbox" id="remember" />
+          <label htmlFor="remember"> Remember me for 30 days</label>
         </div>
-        <div className="btn">
-          {error ? <p className="login-error">{error}</p> : null}
-          <button title="Login" aria-label="Login" type="submit">
-            Login
+        <div>
+          {error ? <p style={styles.error}>{error}</p> : null}
+          <button title="Login" aria-label="Login" type="submit" style={styles.button}>
+            Log in
           </button>
         </div>
+        <div>
+          <Link to="/forgot-password" style = {{textAlign: 'center'}}>Forgot password?</Link>
+        </div>
       </form>
-      <div className="option">
-        <p>
-          Don't have an account? <Link to="/signup">Sign Up</Link>
-        </p>
-      </div>
     </div>
   );
 }
--- react-app/ccf/src/pages/signup.tsx
+++ react-app/ccf/src/pages/signup.tsx
@@ -3,24 +3,37 @@ import { Link } from "react-router-dom";
 import { AuthErrorCodes, createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 
 const Signup: React.FC = () => {
-  const [input, setInput] = useState({ email: "", password: "" });
+  const [input, setInput] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
   const [error, setError] = useState<string | null>(null);
+  const [passwordRequirements, setPasswordRequirements] = useState({
+    hasSpecialChar: false,
+    hasCapitalLetter: false,
+    hasNumber: false,
+  });
 
-  // initialized auth instance
   const auth = getAuth();
 
-  // handle form submit
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setError("");
     const email = input.email.toLowerCase().trim();
     const password = input.password;
 
-    // creating a new user
+    if (password !== input.confirmPassword) {
+      setError("Passwords do not match.");
+      return;
+    }
+
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        // Signed up
         console.log(userCredential.user);
+        console.log("Successfully signed up");
       })
       .catch((err) => {
         if (err.code === AuthErrorCodes.WEAK_PASSWORD) {
@@ -34,58 +47,176 @@ const Signup: React.FC = () => {
       });
   };
 
-  // handle input change
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
     setInput((prevState) => ({
       ...prevState,
-      [e.target.name]: e.target.value,
+      [name]: value,
     }));
+
+    // Check password requirements
+    if (name === "password" || name === "confirmPassword") {
+      setPasswordRequirements({
+        hasSpecialChar: /[!@#$%^&*(),.?":{}|<>]/.test(value),
+        hasCapitalLetter: /[A-Z]/.test(value),
+        hasNumber: /\d/.test(value),
+      });
+    }
+  };
+
+  const styles: { [key: string]: React.CSSProperties } = {
+    signupContainer: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      fontFamily: 'Roboto, sans-serif',
+    },
+    formContainer: {
+      backgroundColor: 'white',
+      padding: '2rem',
+      width: '100%',
+      maxWidth: '400px',
+    },
+    logo: {
+      textAlign: 'center',
+      marginBottom: '1rem',
+    },
+    logoImage: {
+      maxWidth: '100px',
+    },
+    heading: {
+      color: '#333',
+      textAlign: 'center',
+      marginBottom: '1.5rem',
+    },
+    inputGroup: {
+      display: 'flex',
+      gap: '2rem',
+      marginBottom: '1rem',
+    },
+    input: {
+      width: '100%',
+      padding: '0.5rem',
+      marginBottom: '1rem',
+      border: '1px solid #ccc',
+      borderRadius: '4px',
+      marginTop: '0.5rem',
+    },
+    passwordRequirements: {
+      fontSize: '0.8rem',
+      color: '#666',
+      marginBottom: '1rem',
+    },
+    errorMessage: {
+      color: 'red',
+      fontSize: '0.8rem',
+      marginBottom: '1rem',
+    },
+    button: {
+      width: '100%',
+      padding: '0.55rem',
+      backgroundColor: '#c41e3a',
+      color: 'white',
+      border: 'none',
+      borderRadius: '8px',
+      cursor: 'pointer',
+      fontSize: '1rem',
+    },
+    loginLink: {
+      textAlign: 'center',
+      marginTop: '1rem',
+      fontSize: '0.9rem',
+    },
+    loginLinkAnchor: {
+      color: 'blue',
+      textDecoration: 'none',
+    },
   };
 
   return (
-    <div className="form-body">
-      <form autoComplete="off" className="form" onSubmit={handleSubmit}>
-        <h1>Sign Up</h1>
-        <p>Fill the form below to create your account.</p>
-        <div className="email-input">
+    <div style={styles.signupContainer}>
+      <div style={styles.formContainer}>
+        <div style={styles.logo}>
+          <img src="/ccflogo.png" alt="Logo" style={styles.logoImage} />
+        </div>
+        <h1 style={styles.heading}>Create Account</h1>
+        <form autoComplete="off" onSubmit={handleSubmit}>
+          <div style={styles.inputGroup}>
+            <div>
+              <label htmlFor="firstName">First Name</label>
+              <input
+                name="firstName"
+                placeholder="Enter your first name"
+                type="text"
+                onChange={handleChange}
+                value={input.firstName}
+                required
+                style={styles.input}
+              />
+            </div>
+            <div>
+              <label htmlFor="lastName">Last Name</label>
+              <input
+                name="lastName"
+                placeholder="Enter your last name"
+                type="text"
+                onChange={handleChange}
+                value={input.lastName}
+                required
+                style={styles.input}
+              />
+            </div>
+          </div>
+          <label htmlFor="email">Email</label>
           <input
             name="email"
-            placeholder="Enter email"
-            type="text"
+            placeholder="Enter your email"
+            type="email"
             onChange={handleChange}
             value={input.email}
             required
-            autoComplete="true"
+            style={styles.input}
           />
-          <label htmlFor="email" className="label-name">
-            <span className="content-name">Email</span>
-          </label>
-        </div>
-        <div className="password-input">
+          <label htmlFor="password">Password</label>
           <input
             name="password"
-            placeholder="Enter password"
+            placeholder="Create a password"
             onChange={handleChange}
             value={input.password}
             type="password"
             required
-            autoComplete="true"
+            style={styles.input}
           />
-          <label htmlFor="password" className="label-name">
-            <span className="content-name">Password</span>
-          </label>
-        </div>
-        <div className="btn">
-          {error ? <p className="login-error">{error}</p> : null}
-          <button title="Sign up" aria-label="Signup" type="submit">
-            Create account
-          </button>
-        </div>
-      </form>
-      <div className="option">
-        <p>
-          Already have an account?
-          <Link to="/login">Sign in</Link>
+          <div style={styles.passwordRequirements}>
+            <p>Password requires:</p>
+            <ul>
+              <li style={{ color: passwordRequirements.hasSpecialChar ? 'green' : 'red' }}>
+                One special character
+              </li>
+              <li style={{ color: passwordRequirements.hasCapitalLetter ? 'green' : 'red' }}>
+                One capital letter
+              </li>
+              <li style={{ color: passwordRequirements.hasNumber ? 'green' : 'red' }}>
+                One number
+              </li>
+            </ul>
+          </div>
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <input
+            name="confirmPassword"
+            placeholder="Enter password again"
+            onChange={handleChange}
+            value={input.confirmPassword}
+            type="password"
+            required
+            style={styles.input}
+          />
+          {error && <p style={styles.errorMessage}>{error}</p>}
+          <button type="submit" style={styles.button}>Sign Up</button>
+        </form>
+        <p style={styles.loginLink}>
+          Already have an account? <Link to="/login" style={styles.loginLinkAnchor}>Log in</Link>
         </p>
       </div>
     </div>

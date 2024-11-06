5e42ff7bedbf6ae1a9f6f70b6fe3cfe0c0a39919
Advik Duvvuri <123396162+aduvvuri007@users.noreply.github.com>
2024-11-06 18:35:35 -0500

    moved styles to seperate file

--- react-app/ccf/src/deadline-banner/DeadlineBanner.tsx
+++ react-app/ccf/src/deadline-banner/DeadlineBanner.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import './DeadlineBannerStyle.css';
 
 interface BannerProps {
   deadline: Date;
@@ -20,13 +21,10 @@ const Banner: React.FC<BannerProps> = ({ deadline }) => {
         setIsBannerVisible(false);
       }
 
-      setTimeLeft(`due on ${new Date(deadline).toLocaleDateString()}`);
+      setTimeLeft(`DUE ON ${new Date(deadline).toLocaleDateString()}`);
     };
 
     updateTimeLeft();
-    const timer = setInterval(updateTimeLeft, 60000); // update every minute
-
-    return () => clearInterval(timer); // cleanup interval on component unmount
   }, [deadline]);
 
   const handleCloseBanner = () => {
@@ -36,48 +34,17 @@ const Banner: React.FC<BannerProps> = ({ deadline }) => {
   if (!isBannerVisible) return null;
 
   return (
-    <div style={styles.banner}>
-        <div style={styles.textContainer}>
-            <span style={styles.text}>REMINDER: In progress application {timeLeft}!</span>
+    <div className='banner'>
+        <div className='text-container'>
+            <span className='text'><b className='text'>REMINDER:</b> In progress application <b className='text'>{timeLeft}!</b></span>
         </div>
-      <button style={styles.closeButton} onClick={handleCloseBanner}>X</button>
+      <button className='closeButton' onClick={handleCloseBanner}>X</button>
     </div>
   );
 };
 
 Banner.propTypes = {
   deadline: PropTypes.instanceOf(Date).isRequired,
-};
-
-const styles = {
-  banner: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    backgroundColor: 'red',
-    color: 'white',
-    padding: '10px',
-    fontSize: '24px',
-    fontWeight: 'bold',
-    borderRadius: '4px',
-    height: '100px',
-  },
-  textContainer: {
-    display: 'flex',
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: '32px', 
-  },
-  closeButton: {
-    backgroundColor: 'transparent',
-    color: 'white',
-    border: 'none',
-    fontSize: '18px',
-    cursor: 'pointer',
-  },
 };
 
 export default Banner;
